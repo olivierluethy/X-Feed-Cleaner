7b0454f57806d1876464319154968241fcd77662
OlivierLuethy <olivier.luethy@gmx.net>
2024-11-18 20:08:53 +0100

    Hide distractive content on explore page

--- main.js
+++ main.js
@@ -5,21 +5,29 @@ const hideXElements = () => {
   cheatBlocker();
 
   // Start - Remove stuff from navigation
-  const grok = document.querySelector('a[href="/i/grok"][aria-label="Grok"][role="link"]');
-  if(grok){
-    grok.style.display="none";
+  const grok = document.querySelector(
+    'a[href="/i/grok"][aria-label="Grok"][role="link"]'
+  );
+  if (grok) {
+    grok.style.display = "none";
   }
-  const premium = document.querySelector('a[href="/i/premium_sign_up"][aria-label="Premium"][role="link"]');
-  if(premium){
-    premium.style.display="none";
+  const premium = document.querySelector(
+    'a[href="/i/premium_sign_up"][aria-label="Premium"][role="link"]'
+  );
+  if (premium) {
+    premium.style.display = "none";
   }
-  const verifiedOrgs = document.querySelector('a[href="/i/verified-orgs-signup"][aria-label="Verified Orgs"][role="link"]');
-  if(verifiedOrgs){
-    verifiedOrgs.style.display="none";
+  const verifiedOrgs = document.querySelector(
+    'a[href="/i/verified-orgs-signup"][aria-label="Verified Orgs"][role="link"]'
+  );
+  if (verifiedOrgs) {
+    verifiedOrgs.style.display = "none";
   }
-  const notifications = document.querySelector('a[href="/notifications"][aria-label="Notifications"][role="link"]');
-  if(notifications){
-    notifications.style.display="none";
+  const notifications = document.querySelector(
+    'a[href="/notifications"][aria-label="Notifications"][role="link"]'
+  );
+  if (notifications) {
+    notifications.style.display = "none";
   }
 
   // Only hide if the user is on the Home Feed
@@ -103,28 +111,41 @@ const hideXElements = () => {
     }
 
     console.log("Elements on /home page hidden");
-  } else {
-    // Hide the "Live on X" section
-    const liveOnX = document.querySelector(
-      'section[aria-labelledby*="Live"], div[aria-label="Live"]'
+  } else if (path === "/explore") {
+    const explore = document.querySelector(
+      'div[aria-label="Timeline: Explore"]'
     );
-    if (liveOnX) {
-      liveOnX.style.display = "none";
-      console.log("Live on X section hidden outside of /home");
-    } else {
-      console.log("Live on X section not found outside of /home");
+    if (explore) {
+      explore.style.display = "none";
     }
+    const tablist = document.querySelector(
+      'div[role="tablist"][data-testid="ScrollSnap-List"'
+    );
+    if (tablist) {
+      tablist.style.display = "none";
+    }
+  }
 
-    // Hide the "Trends for you" section
-    const trends = document.querySelector(
-      'section[aria-labelledby*="Trends"], div[data-testid="sidebarColumn"]'
-    );
-    if (trends) {
-      trends.style.display = "none";
-      console.log("Trends section hidden outside of /home");
-    } else {
-      console.log("Trends section not found outside of /home");
-    }
+  // Hide the "Live on X" section
+  const liveOnX = document.querySelector(
+    'section[aria-labelledby*="Live"], div[aria-label="Live"]'
+  );
+  if (liveOnX) {
+    liveOnX.style.display = "none";
+    console.log("Live on X section hidden outside of /home");
+  } else {
+    console.log("Live on X section not found outside of /home");
+  }
+
+  // Hide the "Trends for you" section
+  const trends = document.querySelector(
+    'section[aria-labelledby*="Trends"], div[data-testid="sidebarColumn"]'
+  );
+  if (trends) {
+    trends.style.display = "none";
+    console.log("Trends section hidden outside of /home");
+  } else {
+    console.log("Trends section not found outside of /home");
   }
 };
 
