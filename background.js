33ce606208c3b0bd287cb1f72f87bf03a6d538ba
OlivierLuethy <olivier.luethy@gmx.net>
2024-11-30 18:54:54 +0100

    Add page if a new version of the extension is out

--- background.js
+++ background.js
@@ -2,5 +2,9 @@
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === "install") {
     chrome.tabs.create({ url: chrome.runtime.getURL("/pages/downloaded.html") });
+  }else if (details.reason === "update") {
+    chrome.tabs.create({ url: chrome.runtime.getURL("/pages/update.html") });
   }
 });
+// Set a URL to open when the extension is uninstalled
+chrome.runtime.setUninstallURL("https://your-feedback-page.com/uninstall-survey");
